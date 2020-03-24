0041d8681568d76d76ab0f089bb2cce28298e0be
SebiPrelipcean <62348238+SebiPrelipcean@users.noreply.github.com>
2020-03-24 14:39:00 +0200

    Add files via upload
    Tema+tema2+tema3 in proiect

---
+++ Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    class Masina
+    {
+        private string marca;
+        private string culoare;
+        private string model;
+        private double pret;
+        private double an;
+
+        //Proprietati auto-implemented
+        public string MarcaMasina { get; set; }
+        public string CuloaMasina { get; set; }
+        public string ModelMasina { get; set; }
+        public double PretMasina { get; set; }
+        public double AnMasina { get; set; }
+
+
+        //Constructor implicit masina
+        public Masina()
+        {
+            marca = string.Empty;
+            culoare = string.Empty;
+            model = string.Empty;
+            pret = 0;
+            an = 0;
+        }
+
+        //Constructor cu parametri
+        public Masina(string _Marca, string _model, string _culoare, double _pret, double _an)
+        {
+            marca = _Marca;
+            culoare = _culoare;
+            model = _model;
+            pret = _pret;
+            an = _an;
+        }
+
+        public Masina(string date)
+        {
+            string[] ch = date.Split(",");
+            marca = ch[0];
+            model = ch[1];
+            culoare = ch[2];
+            pret = Convert.ToDouble(ch[3]);
+            an = Convert.ToDouble(ch[4]);
+        }
+
+        public Masina CitireTastatura()
+        {
+            Console.WriteLine("Introduceti marca masini: ");
+            string _Mm = Console.ReadLine();
+            Console.WriteLine("Introduceti model: ");
+            string _Md = Console.ReadLine();
+            Console.WriteLine("Introduceti culoarea: ");
+            string _Cc = Console.ReadLine();
+            Console.WriteLine("Introduceti pretul");
+            double _Pp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduceti anul: ");
+            double _Aa = Convert.ToDouble(Console.ReadLine());
+            Masina m = new Masina(_Mm, _Md, _Cc, _Pp, _Aa);
+            return m;
+        }
+
+        public string Info()
+        {
+            return string.Format("{0,0}{1,10}{2,15}{3,10}{4,10}", marca, model, culoare, pret, an);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using Car;
+
+namespace RegistruMasini
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            while (true)
+            {
+                
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("{0 , -10} {1,35} {2,30}", "###", "RegistruMasini v1.0", "###");
+                Console.WriteLine();
+                Console.WriteLine("* A - Vizualizare registru masini");
+                Console.WriteLine("* B - Adaugare masina in registru");
+                Console.WriteLine("* C - Stergere masina din registru");
+                Console.WriteLine("* D - Editare masina din registru ");
+                Console.WriteLine("* E - Cea mai cautata masina intr-o anumita perioada ");
+                Console.WriteLine("* F - Grafic pret pentru o masina din registru");
+                Console.WriteLine("* G - Afisarea tranzactiilor intr-o zi ");
+                Console.WriteLine("* X - Iesire aplicatie!");
+                Console.WriteLine();
+                Console.WriteLine("Introduceti optiunea dorita: ");
+                var a = new Masina("MAZDA","3","NEAGRA",2000,2004);
+                string titlu = string.Format("{0,0}{1,10}{2,15}{3,10}{4,10}\n", "Marca", "Model", "Culoare", "Pret", "An");
+                var b = new Masina("Audi,A5,Gri,2450,2006");
+                string OPT = Console.ReadLine();
+                switch (OPT)
+                {
+                    case "A":
+                    case "a":
+                        string afisare = a.Info();
+                        Console.WriteLine(titlu);
+                        Console.WriteLine(afisare);
+                        Console.WriteLine(b.Info());
+                        Console.ReadKey();
+                        break;
+                    case "B":
+                    case "b":
+                        Console.ReadKey();
+                        break;
+                    case "C":
+                    case "c":
+                        Console.ReadKey();
+                        break;
+                    case "D":
+                    case "d":
+                        Console.ReadKey();
+                        break;
+                    case "E":
+                    case "e":
+                        Console.ReadKey();
+                        break;
+                    case "F":
+                    case "f":
+                        Console.ReadKey();
+                        break;
+                    case "G":
+                    case "g":
+                        Console.ReadKey();
+                        break;
+                    case "X":
+                    case "x":
+                        Console.WriteLine("Doriti sa salvati modificarile? :");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                        break;
+                }
+
+
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
