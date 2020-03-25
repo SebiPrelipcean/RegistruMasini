08778c5b09e4c9bd1d797d63655b968bc6d46a2b
SebiPrelipcean <62348238+SebiPrelipcean@users.noreply.github.com>
2020-03-25 18:23:57 +0200

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -65,7 +65,7 @@ namespace Car
             Masina m = new Masina(_Mm, _Md, _Cc, _Pp, _Aa);
             return m;
         }
-
+        //ConversieLaSir
         public string Info()
         {
             return string.Format("{0,0}{1,10}{2,15}{3,10}{4,10}", marca, model, culoare, pret, an);
