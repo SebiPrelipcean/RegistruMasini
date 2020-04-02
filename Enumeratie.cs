94316b8e1f12be2a078bb920bd8e64d7ece62cbe
SebiPrelipcean <62348238+SebiPrelipcean@users.noreply.github.com>
2020-04-02 16:36:05 +0300

    Add files via upload

---
+++ Enumeratie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    public enum Culoare
+    {
+        Rosu = 1,
+        Galben = 2,
+        Albastru = 3,
+        Verde = 4,
+        Alb = 5,
+        Negreu = 6,
+    };
+}
