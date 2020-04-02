0d7028a8b4ad47277b7a8831519a91211a0960b8
SebiPrelipcean <62348238+SebiPrelipcean@users.noreply.github.com>
2020-04-02 16:34:59 +0300

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -1,16 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Car
 {
+    //public void chenar(Masina[] reg, int nr)
+    //{
+    //    int i;
+    //    Console.BackgroundColor = ConsoleColor.Red;
+    //    for (i = -1; i < nr + 1; i++)
+    //        Console.Write("_");
+    //    for (i = 0; i < nr; i++)
+    //        Console.WriteLine("| " + reg[i].Info() + " |");
+    //    for (i = -1; i < nr + 1; i++)
+    //        Console.Write("_");
+
+
+
+    //}
     class Masina
     {
         private string marca;
-        private string culoare;
+        //private string culoare;
         private string model;
         private double pret;
         private double an;
+        private Culoare culoare { get; set; }
+
+
 
         //Proprietati auto-implemented
         public string MarcaMasina { get; set; }
@@ -24,14 +41,14 @@ namespace Car
         public Masina()
         {
             marca = string.Empty;
-            culoare = string.Empty;
+            culoare = Culoare.Alb;
             model = string.Empty;
             pret = 0;
             an = 0;
         }
 
         //Constructor cu parametri
-        public Masina(string _Marca, string _model, string _culoare, double _pret, double _an)
+        public Masina(string _Marca, string _model, Culoare _culoare, double _pret, double _an)
         {
             marca = _Marca;
             culoare = _culoare;
@@ -45,7 +62,8 @@ namespace Car
             string[] ch = date.Split(",");
             marca = ch[0];
             model = ch[1];
-            culoare = ch[2];
+            int help = Convert.ToInt32(ch[2]);
+            culoare = (Culoare)help;
             pret = Convert.ToDouble(ch[3]);
             an = Convert.ToDouble(ch[4]);
         }
@@ -56,8 +74,15 @@ namespace Car
             string _Mm = Console.ReadLine();
             Console.WriteLine("Introduceti model: ");
             string _Md = Console.ReadLine();
+            Console.WriteLine("1 - Rosu \n" +
+            "2 - Galben \n" +
+            "3 - Albastru \n" +
+            "4 - Verde \n" +
+            "5 - Alb \n" +
+            "6 - Negru \n");
             Console.WriteLine("Introduceti culoarea: ");
-            string _Cc = Console.ReadLine();
+            int optt = Convert.ToInt32(Console.ReadLine());
+            Culoare _Cc =  (Culoare)optt;
             Console.WriteLine("Introduceti pretul");
             double _Pp = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Introduceti anul: ");
@@ -65,10 +90,42 @@ namespace Car
             Masina m = new Masina(_Mm, _Md, _Cc, _Pp, _Aa);
             return m;
         }
-        //ConversieLaSir
+
+        public bool ComparareMasini(Masina a2)
+        {
+            if (string.Equals(marca, a2.marca))
+                return true;
+            else return false;
+        }
+
         public string Info()
         {
-            return string.Format("{0,0}{1,10}{2,15}{3,10}{4,10}", marca, model, culoare, pret, an);
+            return string.Format("{0,5}{1,10}{2,15}{3,10}{4,10}", marca, model, culoare, pret, an);
+        }
+
+        public  Masina Atribbuit (Masina x)
+        {
+            x.marca = marca;
+            x.pret = pret;
+            x.model = model;
+            x.culoare = culoare;
+            x.an = an;
+            return x;
+        }
+
+        public void chenar(Masina[] reg, int nr)
+        {
+            int i;
+            Console.BackgroundColor = ConsoleColor.Red;
+            for (i = -1; i < nr + 1; i++)
+                Console.Write("_");
+            for (i = 0; i < nr; i++)
+                Console.WriteLine("| " + reg[i].Info() + " |");
+            for (i = -1; i < nr + 1; i++)
+                Console.Write("_");
+
+
+
         }
     }
 }
